15d66f5ca880d52a1a14a48a0c3f664fcbc46497
Durgaravi-04 <durgaravi028@gmail.com>
2025-08-13 11:21:56 +0530

    javascript

---
+++ javascript/code.js
@@ -0,0 +1,25 @@
+//Even and Odd
+let n = 10;
+for (let i = 1; i <= n; i = i + 2) {
+    console.log(i)
+}
+for (let i = 2; i <= n; i = i + 2) {
+    console.log(i)
+}
+//Print 1 to n
+let a = 5;
+for (i = 1; i <= a; i++) {
+    console.log(i)
+}
+//Multipling
+let c = 5;
+for (i = 1; i <= 10; i++) {
+    mul = c * i;
+    console.log(mul);
+}
+//Sum
+let d = 5, sum = 0;
+for (i = 1; i <= d; i++) {
+    sum = sum + i;
+}
+console.log(sum);
