ed9532d2ed1633a33716ffe351a8eaebfacbcf2f
Durgaravi-04 <durgaravi028@gmail.com>
2025-08-22 18:33:39 +0530

    color

---
+++ final.js
@@ -0,0 +1,39 @@
+const con = document.getElementById("container")
+
+const arr = ["./assets/black.jpg",
+    "./assets/sports.jpg",
+    "./assets/grey.jpg",
+    "./assets/white.jpg",
+    "./assets/blue.jpg"
+];
+const img = document.getElementsByClassName("img")
+console.log(img);
+
+let i = 0;
+con.style.backgroundImage = `url(${arr[i]})`;
+
+
+let changeImg1 = () => {
+    img[i].classList.remove("active")
+    i++;
+    if (i == arr.length) {
+        i = 0;
+    }
+    img[i].classList.add("active")
+    con.style.backgroundImage = `url(${arr[i]})`;
+
+}
+let changeImg2 = () => {
+    i--;
+    if (i == -1) {
+        i = arr.length - 1;
+    }
+    con.style.backgroundImage = `url(${arr[i]})`;
+}
+let change = (a) => {
+    con.style.backgroundImage = `url(${arr[a]})`;
+}
+
+// setInterval(() => {
+//     changeImg1();
+// }, 2000);
